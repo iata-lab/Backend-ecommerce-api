09a8cda74a23f8a5c5e547aae94c2d800fb96a7c
MrCamoga <mrcamoga@gmail.com>
2025-05-29 18:57:12 +0200

    Endpoints GET, PUT y DELETE

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 app.use(express.json());
 app.get("/", (req,res) => res.send("Test"));
 
-
+app.use("/products", require("./routes/products"));
 
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
